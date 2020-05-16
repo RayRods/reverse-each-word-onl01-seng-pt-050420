bd990e5b7819f4dfef0ffe0a7c8d2cf9b0f568ff
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-16 08:13:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def reverse_each_word(sentence1)
   new_array = []
   word_array = sentence1.split(" ")
@@ -10,4 +11,14 @@ def reverse_each_word(sentence2)
   word1_array = sentence2.split(" ")
   word1_array.collect {|strings2| new1_array << strings2.reverse}
  return new1_array.join(" ")
+=======
+def reverse_each_word(phrase = "I love my homie tons. She is the best!")
+  phrase.each do |words|
+    word.join(" ")
+    rev_sentence = word.reverse
+    
+  end
+    
+    
+>>>>>>> 2fa6d84e9a4208e5f63841468498f4dcb14767eb
 end
